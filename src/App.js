0919006a133a398d755a541ec7a5490a6bea26b8
zzzzzkjs <zkjs@naver.com>
2020-07-05 20:44:32 +0900

    컴포넌트 테스트

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import Test from './components/Test';
+
+function InnerComponent() {
+  return <h3>내부 컴포넌트(InnerComponent) 생성</h3>;
+}
 
 function App() {
-  return <div>리액트 앱 테스트!</div>;
+  return (
+    <div>
+      <h1>리액트 앱 테스트!!!!</h1>
+      <Test />
+      <InnerComponent />
+    </div>);
 }
 
 export default App;
